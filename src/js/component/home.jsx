fa11284a2e3211f9a2803ba1bdc9d5f9addb9f58
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-11-27 02:35:09 +0000

    first committ

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,21 +8,9 @@ const Home = () => {
 		<>
 			<div id="trafficTop"></div>
 			<div id="container">
-				<div
-					onClick={() => setLitUp("red")}
-					className={
-						"light red" + (litUp === "red" ? " selected" : "")
-					}></div>
-				<div
-					onClick={() => setLitUp("yellow")}
-					className={
-						"light yellow" + (litUp === "yellow" ? " selected" : "")
-					}></div>
-				<div
-					onClick={() => setLitUp("green")}
-					className={
-						"light green" + (litUp === "green" ? " selected" : "")
-					}></div>
+				<div onClick={() => setLitUp("red")} className={"light red" + (litUp === "red" ? " selected" : "")}></div>
+				<div onClick={() => setLitUp("yellow")} className={"light yellow" + (litUp === "yellow" ? " selected" : "")}></div>
+				<div onClick={() => setLitUp("green")} className={"light green" + (litUp === "green" ? " selected" : "")}></div>
 			</div>
 		</>
 	);
